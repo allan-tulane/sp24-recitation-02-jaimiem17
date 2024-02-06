f5e405162502f848fdd622caad301e75edb2a9c8
jaimiem17 <79995651+jaimiem17@users.noreply.github.com>
2024-02-06 20:10:20 +0000

    Update

--- main.py
+++ main.py
@@ -5,100 +5,123 @@ CMPS 2200  Recitation 2
 ### the only imports needed are here
 import tabulate
 import time
+
+#import tabulate
 ###
 
+
 def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
+  if n <= 1:
+    return 1
+  else:
+    return (a * simple_work_calc(n // b, a, b) + n)
+  # pass
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
 
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+"""Compute the value of the recurrence $W(n) = aW(n/b) + n
+
+  Params:
+  n......input integer
+  a......branching factor of recursion tree
+  b......input split factor
+
+  Returns: the value of W(n).
+  """
+
+# TODO
+
 
 def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
+  if n <= 1:
+    return 1
+  else:
+    return (a * work_calc(n // b, a, b, f) + f(n))
+  """Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
+  Params:
+  n......input integer
+  a......branching factor of recursion tree
+  b......input split factor
+  f......a function that takes an integer and returns 
            the work done at each node 
 
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  Returns: the value of W(n).
+  """
+  # TODO
+
+  pass
+
 
 def span_calc(n, a, b, f):
-	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
+  """Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
+  Params:
+  n......input integer
+  a......branching factor of recursion tree
+  b......input split factor
+  f......a function that takes an integer and returns 
            the work done at each node 
 
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  Returns: the value of W(n).
+  """
+  # TODO
+  if n == 1:
+    return 1
+  else:
+    return (
+        span_calc(n // b, a, b, f) + f(n)
+    )  #dont multiply by a - only evaluating longest branch not all nodes
+
+  #pass
 
 
+def compare_work(work_fn1,
+                 work_fn2,
+                 sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+  """
+  Compare the values of different recurrences for 
+  given input sizes.
 
-def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-	"""
-	Compare the values of different recurrences for 
-	given input sizes.
+  Returns:
+  A list of tuples of the form
+  (n, work_fn1(n), work_fn2(n), ...)
 
-	Returns:
-	A list of tuples of the form
-	(n, work_fn1(n), work_fn2(n), ...)
-	
-	"""
-	result = []
-	for n in sizes:
-		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
-	return result
+  """
+
+  result = []
+  for n in sizes:
+
+    # compute W(n) using current a, b, f
+    result.append((n, work_fn1(n), work_fn2(n)))
+  #print_results(result)
+  return result
+
 
 def print_results(results):
-	""" done """
-	print(tabulate.tabulate(results,
-							headers=['n', 'W_1', 'W_2'],
-							floatfmt=".3f",
-							tablefmt="github"))
+  """ done """
+  print(
+      tabulate.tabulate(results,
+                        headers=['n', 'W_1', 'W_2'],
+                        floatfmt=".3f",
+                        tablefmt="github"))
 
 
+def compare_span(span_fn1,
+                 span_fn2,
+                 sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+  """
+  Compare the values of different recurrences for 
+  given input sizes.
 
-def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-	"""
-	Compare the values of different recurrences for 
-	given input sizes.
+  Returns:
+  A list of tuples of the form
+  (n, work_fn1(n), work_fn2(n), ...)
 
-	Returns:
-	A list of tuples of the form
-	(n, work_fn1(n), work_fn2(n), ...)
-	
-	"""
-	result = []
-	for n in sizes:
-		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			span_fn1,
-			span_fn2
-			))
-	return result
-	
+  """
+  result = []
+  for n in sizes:
+    # compute W(n) using current a, b, f
+    result.append((n, span_fn1(n), span_fn2(n)))
 
+  #print_results(result)
+  return result
--- test_main.py
+++ test_main.py
@@ -1,27 +1,68 @@
 from main import *
+import math
+
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+  assert simple_work_calc(10, 2, 2) == 36
+  assert simple_work_calc(20, 3, 2) == 230
+  assert simple_work_calc(30, 4, 2) == 650
+
+  # added 3 additional tests below
+  assert simple_work_calc(10, 4, 2) == 126
+  assert simple_work_calc(20, 2, 2) == 92
+  assert simple_work_calc(30, 3, 2) == 300
+  """ done. """
+
 
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+  assert work_calc(10, 2, 2, lambda n: 1) == 15
+  assert work_calc(20, 1, 2, lambda n: n * n) == 530
+  assert work_calc(30, 3, 2, lambda n: n) == 300
+
+  # added 3 additional tests below
+  assert work_calc(10, 2, 2, lambda n: n + 2) == 50
+  assert work_calc(10, 2, 2, lambda n: n % 2) == 10
+  assert work_calc(30, 1, 2, lambda n: n * 2) == 111
+  #
+  #assert work_calc(1000, 1, 2, lambda n: n) == 10
+
+
+n = 200
+print(work_calc(n, 2, 2, lambda n: 1))
+print(work_calc(n, 2, 2, lambda n: math.log(n)))
+print(work_calc(n, 2, 2, lambda n: n))
 
 
 def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
+  # curry work_calc to create multiple work -> function inside function
+  # functions that can be passed to compare_work
+  def work_fn1(n):
+    return work_calc(n, 4, 2, lambda n: n)  # linear
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+  # create work_fn2
+  def work_fn2(n):
+    return work_calc(n, 4, 2, lambda n: n * n * n)  #n^3
 
-	
+  # pass
+  res = compare_work(work_fn1, work_fn2)
+  print_results(res)
+
+
+test_compare_work()
+
+
 def test_compare_span():
-	# TODO
+  # create work_fn1
+  def span_fn1(n):
+    return span_calc(n, 4, 2, lambda n: n)
+
+  # create work_fn2
+  def span_fn2(n):
+    return span_calc(n, 4, 2, lambda n: n * n * n)
+
+  res = compare_span(span_fn1, span_fn2)
+  print_results(res)
+  #pass
+
+
+test_compare_span()
